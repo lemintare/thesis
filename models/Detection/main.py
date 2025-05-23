aa8f8429e618f19e07a32cf1979cfc5437b64c2b
lemintare <lemintare@eduard.net.kht.ru>
2025-05-23 15:53:03 +1000

    Убрал сохранение в ONNX модели детекции

--- models/Detection/main.py
+++ models/Detection/main.py
@@ -21,17 +21,6 @@ def main():
     )
     print(f"Training finished. Weights saved to {project_dir}/{run_name}/weights/best.pt")
 
-    best_pt = os.path.join(project_dir, run_name, 'weights', 'best.pt')
-    onnx_out = os.path.join(project_dir, run_name, 'weights', 'best.onnx')
-
-    model_for_export = YOLO(best_pt)
-    model_for_export.export(
-        format = 'onnx',
-        imgsz  = imgsz,
-        opset  = 12,       
-        dynamic = True     
-    )
-    print(f"ONNX model saved to {onnx_out}")
 
 if __name__ == '__main__':
     main()
